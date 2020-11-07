50d6215c65df0f7b65b02d1d714a723cff73dec9
KanishkaRepo <70097774+KanishkaRepo@users.noreply.github.com>
2020-11-07 06:54:06 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,69 @@
+//Create variables here
+var dog, happyDog;
+var foodS, foodStock;
+var database;
+var dogImage, happyDogImage;
+
+function preload()
+{
+  //load images here
+   dogImage = loadImage("images/dogImg.png");
+   happyDogImage = loadImage("images/dogImg1.png");
+
+}
+
+function setup() {
+  createCanvas(500, 500);
+  
+  database = firebase.database();
+
+     dog = createSprite(200, 200, 80, 20);
+     dog .addImage(dogImage);
+     dog.scale = 0.5;
+
+    foodStock = database.ref('food');
+    foodStock.on("value", readStock);
+}
+
+
+function draw() {  
+
+  background(46, 139, 87);
+
+  if(keyWentDown(UP_ARROW)){
+    writeStock(foodS);
+    dog.addImage(happyDogImage);
+  }
+ 
+  dog.display();
+ 
+  drawSprites();
+  //add styles here
+  stroke(20);
+  textSize(15)
+  fill("white")
+  text("foodStock:"+foodS, 120, 200);
+  text("press UP ARROW to feed the dog", 100, 150);
+
+}
+
+function readStock(data){
+  foodS = data.val();
+
+}
+
+function writeStock(x){
+  if(x<=0){
+    x = 0;
+  }
+  else{
+    x = x-1;
+  }
+
+  database.ref('/').update({
+  food:x  
+  })
+
+  
+}
+
